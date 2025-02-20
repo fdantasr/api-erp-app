65c4ffcb8491cadf2bb60ef43924f3cd0a5b6bf3
Felipe Dantas <dantas.contatod@gmail.com>
2025-02-20 20:36:56 -0300

    Update auth.py

--- accounts/auth.py
+++ accounts/auth.py
@@ -6,20 +6,22 @@ from companies.models import Enterprise, Employee
 class Authentication:
 
     def signin(self, email=None, password=None) -> User:
-        excpetion_auth = AuthenticationFailed('Email e/ou senha incorreto(s)')
-        user_exists=User.objects.filter(email=email).exists()
-
-        if not user_exists:
-            raise  excpetion_auth
+        exception_auth = AuthenticationFailed('Email e/ou senha incorreto(s)')
         
+        # Verificando se o usuário existe
         user = User.objects.filter(email=email).first()
 
+        if not user:
+            raise exception_auth
+        
+        # Verificando a senha
         if not check_password(password, user.password):
-            raise excpetion_auth 
+            raise exception_auth 
+        
         return user
     
     def signup(self, name, email, password, type_account="owner", company_id=False):
-        #Validações dos campos 
+        # Validações dos campos 
         if not name or name == '':
             raise APIException('Nome não pode ser Null')
         if not email or email == '':
@@ -28,30 +30,34 @@ class Authentication:
             raise APIException('Password não pode ser Null')
         if type_account == 'employee' and not company_id:
             raise APIException('ID da empresa não pode ser Null')
-            
-        user = User
-        if user.objects.filter(email=email).exists():
+        
+        # Verificando se o email já está cadastrado
+        if User.objects.filter(email=email).exists():
             raise APIException('Email já cadastrado')
         
-        #Criptografando a senha
+        # Criptografando a senha
         password_hashed = make_password(password)
 
-        created_user = user.objetcts.create(
+        # Criando o usuário
+        created_user = User.objects.create(
             name=name,
             email=email,
             password=password_hashed,
             is_owner=0 if type_account == 'employee' else 1
         )
-        #Criando a empresa 
+
+        # Criando a empresa se for o owner
         if type_account == 'owner':
-           created_enterprise = Enterprise.objects.create(
-                name="Nome da empresa",
+            created_enterprise = Enterprise.objects.create(
+                name="Nome da empresa",  
                 user_id=created_user.id
             )
-        #Criando o funcionário
+        
+        # Criando o funcionário se for o employee
         if type_account == 'employee':
             Employee.objects.create(
-                  user_id=created_user.id,
-                  enterprise_id=company_id or created_enterprise.id
+                user_id=created_user.id,
+                enterprise_id=company_id or created_enterprise.id
             )
+
         return created_user
