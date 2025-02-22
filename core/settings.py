fd5794302444ce4535e8df35696a01361ba20af6
Felipe Dantas <dantas.contatod@gmail.com>
2025-02-22 04:48:42 -0300

    a

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("API_URL", "").split(",")
+ALLOWED_HOSTS = [os.getenv("API_URL", "localhost")]
 
 # Application definition
 
